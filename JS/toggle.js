9f16d4269d56c3f53a3071b73d089d9b90362f87
vakho1995 <vakhogabedava@gmail.com>
2025-07-03 22:31:11 +0400

    +

--- JS/toggle.js
+++ JS/toggle.js
@@ -1,11 +1,24 @@
 const burger = document.querySelector(".burger");
-const nuv = document.querySelector(".mobile-nav");
+const nav = document.querySelector(".mobile-nav");
 const burger1 = document.querySelector(".burger-icon1");
 const burger2 = document.querySelector(".burger-icon2");
 
 burger.addEventListener("click", () => {
-  nuv.classList.toggle("is-open");
+  nav.classList.toggle("is-open");
 
-  burger1.style.display = nuv.classList.contains("is-open") ? "none" : "block";
-  burger2.style.display = nuv.classList.contains("is-open") ? "block" : "none";
+  burger1.style.display = nav.classList.contains("is-open") ? "none" : "block";
+  burger2.style.display = nav.classList.contains("is-open") ? "block" : "none";
 });
+
+///if window width more than 768px,
+// the "function handleMediaChange()" removes class "is-open"
+// from navigation-bar for mobile.
+const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+function handleMediaChange(e) {
+  if (!e.matches) {
+    nav.classList.remove("is-open");
+  }
+}
+mediaQuery.addEventListener("change", handleMediaChange);
+handleMediaChange(mediaQuery);
