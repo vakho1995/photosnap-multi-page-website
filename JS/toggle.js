5f2bf5e982b75312950a266b41049c5a268d1970
vakho1995 <vakhogabedava@gmail.com>
2025-07-03 04:28:05 +0400

    add responsive, change class names and mixins

--- JS/toggle.js
+++ JS/toggle.js
@@ -1,11 +1,11 @@
 const burger = document.querySelector(".burger");
-const nuv = document.querySelector(".hide-nav-for-desktop");
+const nuv = document.querySelector(".mobile-nav");
 const burger1 = document.querySelector(".burger-icon1");
 const burger2 = document.querySelector(".burger-icon2");
 
 burger.addEventListener("click", () => {
-  nuv.classList.toggle("open");
+  nuv.classList.toggle("is-open");
 
-  burger1.style.display = nuv.classList.contains("open") ? "none" : "block";
-  burger2.style.display = nuv.classList.contains("open") ? "block" : "none";
+  burger1.style.display = nuv.classList.contains("is-open") ? "none" : "block";
+  burger2.style.display = nuv.classList.contains("is-open") ? "block" : "none";
 });
